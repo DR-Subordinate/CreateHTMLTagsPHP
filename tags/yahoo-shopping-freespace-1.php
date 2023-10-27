48a0bd8b229d894bb07fac7b89376b3c6f1b2fed
DR Subordinate <dr_subordinate@example.com>
2023-10-27 17:11:30 +0900

    ヤフショ＆ヤフオク用説明文の修正
    <BR>と<CENTER>をそれぞれ小文字にする
    オリジナルは大文字だったが修正の指示に従い変更

--- tags/yahoo-shopping-freespace-1.php
+++ tags/yahoo-shopping-freespace-1.php
@@ -1,5 +1,5 @@
 <?php if(!empty($_POST['img-url'])): ?>
-<BR><BR><CENTER>
+<br><br><center>
 <a href="https://shopping.yahoo.co.jp/stores/wrap/bouncer.html?dest_path=https://brandacross.xsrv.jp/syousaigazou/<?php echo $_POST['img-url']; ?>.html" target="_blank">
 <font size="6">
 <img src="https://auctions.c.yimg.jp/images.auctions.yahoo.co.jp/image/dr001/snavi0/ez4ai2mjspd322phnvvsooaa5m/2095788753.jpg" width="80%"></a>

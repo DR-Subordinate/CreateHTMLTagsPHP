3b5ee909ce6eed98e3807d51f909309f43b2cd72
DR Subordinate <dr_subordinate@example.com>
2023-10-30 16:29:00 +0900

    スマホ画像HTML用の修正
    JavaScriptファイルのパスをCDNに変更
    着実にJavaScriptを読み込ませるため

--- tags/sp-img-html.php
+++ tags/sp-img-html.php
@@ -8,9 +8,9 @@
 #galleria{height:430px}
 </style>
 <!-- load jQuery -->
-<script src="../dist/jquery.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <!-- load Galleria -->
-<script src="../dist/galleria-1.3.6.min.js?121"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/galleria/1.3.6/galleria.min.js"></script>
 </head>
 <body>
 <div style="font-size:14px; color:#333; margin:0; padding:10px 20px;">商品詳細写真</div>

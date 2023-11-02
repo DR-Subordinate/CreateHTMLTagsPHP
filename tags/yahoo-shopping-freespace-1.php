c12a99b3eb196e8dd7af2ee5051f98f89847a388
DR Subordinate <dr_subordinate@example.com>
2023-11-02 15:19:16 +0900

    ヤフショ＆ヤフオク用説明文の修正
    コンディション部の画像のサイズをテーブルの横幅と合わせるために
    width="80%"にする

--- tags/yahoo-shopping-freespace-1.php
+++ tags/yahoo-shopping-freespace-1.php
@@ -191,7 +191,7 @@
 </table>
  <br>
 <br />
-<img src="https://shopping.c.yimg.jp/lib/brand-across/title_item_2.jpg" alt="商品ランク" style="vertical-align: bottom" width="350">
+<img src="https://shopping.c.yimg.jp/lib/brand-across/title_item_2.jpg" alt="商品ランク" style="vertical-align: bottom" width="80%">
 <br />
 <table width="80%" bgcolor="#dddddd" cellspacing="1" cellpadding="10" style="border-collapse:collapse;">
 <tr>

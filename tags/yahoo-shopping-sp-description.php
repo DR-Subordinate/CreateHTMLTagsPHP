a5841d42f9be4216d7122feb8fb6827860833165
DR Subordinate <dr_subordinate@example.com>
2023-11-05 16:27:40 +0900

    Yahoo!ショッピング スマホ用説明文の修正
    修正依頼通りに詳細画像はコチラをクリックとITEM SPECの画像
    に隙間を作る

--- tags/yahoo-shopping-sp-description.php
+++ tags/yahoo-shopping-sp-description.php
@@ -1,5 +1,5 @@
 <?php if(!empty($_POST['img-url'])): ?>
-<a href="https://shopping.yahoo.co.jp/stores/wrap/bouncer.html?dest_path=https://brandacross.xsrv.jp/syousaigazou/<?php echo $_POST['img-url']; ?>.html" target="_blank">
+<a href="https://shopping.yahoo.co.jp/stores/wrap/bouncer.html?dest_path=https://brandacross.xsrv.jp/syousaigazou/<?php echo $_POST['img-url']; ?>.html" target="_blank" style="margin-bottom: 20px; display: block;">
 <img src="https://auctions.c.yimg.jp/images.auctions.yahoo.co.jp/image/dr001/snavi0/ez4ai2mjspd322phnvvsooaa5m/2095788753.jpg" border="0" width="100%"></a>
 </a>
 <br>

4d30f4de8c7c5c2031033b6c5e7e7102e4b29eb3
DR Subordinate <dr_subordinate@example.com>
2023-11-09 15:39:33 +0900

    Yahoo!ショッピング スマホ用説明文の修正
    修正依頼通りにRANKテーブルの<th>と<td>を変更

--- tags/yahoo-shopping-sp-description.php
+++ tags/yahoo-shopping-sp-description.php
@@ -164,8 +164,8 @@
 <img src="https://shopping.c.yimg.jp/lib/brand-across/title_item_2.jpg" style="vertical-align: bottom" alt="商品ランク" width="100%">
 <table class="tblitemrank" width="100%" style="border-collapse:collapse;">
 <tr>
-<th bgcolor="#EBE8D7" width="20%" style="font-size:24px;font-weight:bold;color:#bf0000; padding:8px; text-align:center;border:#dddddd solid 1px;" ><?php echo $_POST['condition']; ?></th>
-<td bgcolor="#ffffff" width="80%" style="border:#dddddd solid 1px; vertical-align: middle; padding: 8px;">
+<th width="20%" bgcolor="#EBE8D7" style="padding:8px;border:#dddddd solid 1px;font-size:24px;font-weight:bold;color:#bf0000;"><?php echo $_POST['condition']; ?></th>
+<td width="80%" bgcolor="#ffffff" style="padding:8px;border:#dddddd solid 1px;">
 <?php 
 switch ($_POST['condition']) {
     case "N":
@@ -199,20 +199,20 @@ switch ($_POST['condition']) {
 ?></td></tr>
 <?php if(!empty($_POST['outside'])): ?>
 <tr>
-<th bgcolor="#EBE8D7" width="20%" style="text-align:center;border:#dddddd solid 1px; vertical-align: middle; padding:8px;">外側</th>
-<td bgcolor="#ffffff" width="80%" style="border:#dddddd solid 1px; padding: 8px;"><?php echo nl2br($_POST['outside']); ?></td>
+<th width="20%" bgcolor="#EBE8D7" style="padding:8px;border:#dddddd solid 1px;">外側</th>
+<td width="80%" bgcolor="#ffffff" style="padding:8px;border:#dddddd solid 1px;"><?php echo nl2br($_POST['outside']); ?></td>
 </tr>
 <?php endif; ?>
 <?php if(!empty($_POST['inside'])): ?>
 <tr>
-<th bgcolor="#EBE8D7" width="20%" style="text-align:center;border:#dddddd solid 1px; vertical-align: middle; padding:8px;">内側</th>
-<td bgcolor="#ffffff" width="80%" style="border:#dddddd solid 1px; padding: 8px;"><?php echo nl2br($_POST['inside']); ?></td>
+<th width="20%" bgcolor="#EBE8D7" style="padding:8px;border:#dddddd solid 1px;">内側</th>
+<td width="80%" bgcolor="#ffffff" style="padding:8px;border:#dddddd solid 1px;"><?php echo nl2br($_POST['inside']); ?></td>
 </tr>
 <?php endif; ?>
 <?php if(!empty($_POST['special-note'])): ?>
 <tr>
-<th bgcolor="#EBE8D7" width="20%" style="text-align:center;border:#dddddd solid 1px; vertical-align: middle; padding:8px;">特記</th>
-<td bgcolor="#ffffff" width="80%" style="border:#dddddd solid 1px; padding: 8px;"><?php echo nl2br($_POST['special-note']); ?></td>
+<th width="20%" bgcolor="#EBE8D7" style="padding:8px;border:#dddddd solid 1px;">特記</th>
+<td width="80%" bgcolor="#ffffff" style="padding:8px;border:#dddddd solid 1px;"><?php echo nl2br($_POST['special-note']); ?></td>
 </tr>
 <?php endif; ?>
 <?php if(!empty($_POST['syousai'])): ?>

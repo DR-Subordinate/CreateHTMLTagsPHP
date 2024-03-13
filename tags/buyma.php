65488c0a5817463f3a5dfb5f412114c930933008
DR Subordinate <dr_subordinate@example.com>
2024-03-13 15:00:03 +0900

    BUYMA用の修正
    状態、外側、内側、特記を追加

--- tags/buyma.php
+++ tags/buyma.php
@@ -63,6 +63,57 @@
 　　　　　', $_POST['accessory']); ?>
 
 <?php endif; ?>
+<?php if(!empty($_POST['condition'])): ?>
+状態　　<?php echo $_POST['condition']; ?>　　<?php endif; ?>
+<?php 
+
+switch ($_POST['condition']) {
+    case "N":
+        echo "（新品、未使用品  製造から2年以内の商品）";
+        break;
+    case "NS":
+        echo "（新品、未使用品 製造から2年以上経過、又は製造年が不明な商品）";
+        break;
+    case "S":
+        echo "（新品同様品）";
+        break;
+    case "A":
+        echo "（数回使用程度の美品）";
+        break;
+    case "AB":
+        echo "（若干の小傷又はシミがあるが全体的には状態が良い）";
+        break;
+    case "B":
+        echo "（一般的な中古品で全体的に使用感がある）";
+        break;
+    case "BC":
+        echo "（全体的に強い使用感がある）";
+        break;
+    case "C":
+        echo "（強い使用感あり　劣化や大きい汚れなどがある）";
+        break;
+    case "D":
+        echo "（欠品、故障有り）";
+        break;
+}
+?>
+
+
+<?php if(!empty($_POST['outside'])): ?>
+外　側　　<?php echo str_replace(array("\n"), '
+　　　　　', $_POST['outside']); ?>
+
+<?php endif; ?>
+<?php if(!empty($_POST['inside'])): ?>
+内　側　　<?php echo str_replace(array("\n"), '
+　　　　　', $_POST['inside']); ?>
+
+<?php endif; ?>
+<?php if(!empty($_POST['special-note'])): ?>
+特　記　　<?php echo str_replace(array("\n"), '
+　　　　　', $_POST['special-note']); ?>
+
+<?php endif; ?>
 <?php if(!empty($_POST['syousai'])): ?>
 詳　細　　<?php echo $_POST['syousai']; ?>
 

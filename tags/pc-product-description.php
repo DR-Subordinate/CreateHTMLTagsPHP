a643d8b394da3dc427c770211dc05aecccdeb9ea
DR Subordinate <dr_subordinate@example.com>
2024-10-13 22:51:38 +0900

    PC用商品説明文の修正
    NS(未使用展示品)を表示させるように修正

--- tags/pc-product-description.php
+++ tags/pc-product-description.php
@@ -208,6 +208,9 @@ switch ($_POST['condition']) {
     case "NS":
         echo "新品、未使用品 製造から2年以上経過、又は製造年が不明な商品";
         break;
+    case "NS-display-item":
+        echo "未使用展示品";
+        break;
     case "S":
         echo "新品同様品";
         break;
@@ -261,6 +264,7 @@ switch ($_POST['condition']) {
 	<tr>
 		<th>N</th>
 		<th>NS</th>
+		<th>NS(未使用展示品)</th>
 		<th>S</th>
 		<th>A</th>
 		<th>AB</th>
@@ -272,6 +276,7 @@ switch ($_POST['condition']) {
 	<tr>
 		<td<?php if($_POST['condition'] == "N"):?> bgcolor="#FFF766"<?php endif; ?>>新<br>品</td>
 		<td<?php if($_POST['condition'] == "NS"):?> bgcolor="#FFF766"<?php endif; ?>>未<br>使<br>用<br>品</td>
+		<td<?php if($_POST['condition'] == "NS-display-item"):?> bgcolor="#FFF766"<?php endif; ?>>未<br>使<br>用<br>展<br>示<br>品</td>
 		<td<?php if($_POST['condition'] == "S"):?> bgcolor="#FFF766"<?php endif; ?>>新<br>品<br>同<br>様<br>品</td>
 		<td<?php if($_POST['condition'] == "A"):?> bgcolor="#FFF766"<?php endif; ?>>中<br>古<br>超<br>美<br>品</td>
 		<td<?php if($_POST['condition'] == "AB"):?> bgcolor="#FFF766"<?php endif; ?>>中<br>古<br>美<br>品</td>

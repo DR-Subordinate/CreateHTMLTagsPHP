4cc567f019d83dc811d83e28127ffed4af0bcf65
DR Subordinate <dr_subordinate@example.com>
2024-11-14 22:18:18 +0900

    PC用商品説明文の修正
    新しいランクの追加と既存のランクの説明変更

--- tags/pc-product-description.php
+++ tags/pc-product-description.php
@@ -203,13 +203,16 @@ switch ($_POST['country-of-manufacture']) {
 <?php 
 switch ($_POST['condition']) {
     case "N":
-        echo "新品、未使用品  製造から2年以内の商品";
+        echo "新品※金具部分など輸送・検品の際にどうしてもついてしまう僅かなスレ傷がある場合がございます。";
         break;
     case "NS":
-        echo "新品、未使用品 製造から2年以上経過、又は製造年が不明な商品";
+        echo "未使用品 付属品に欠品あり又は製造年が不明な商品※保管による僅かなダメージがある場合がございます。";
         break;
     case "S":
-        echo "新品同様品";
+        echo "展示品またはアウトレット商品 未使用品だが長期保管や展示により明確なダメージ等がある新古商品";
+        break;
+    case "SA":
+        echo "新品同様品 細かなあまり目立たないダメージがある程度の新品同様に奇麗なUSED商品";
         break;
     case "A":
         echo "数回使用程度の美品";
@@ -262,6 +265,7 @@ switch ($_POST['condition']) {
 		<th>N</th>
 		<th>NS</th>
 		<th>S</th>
+		<th>SA</th>
 		<th>A</th>
 		<th>AB</th>
 		<th>B</th>
@@ -272,7 +276,8 @@ switch ($_POST['condition']) {
 	<tr>
 		<td<?php if($_POST['condition'] == "N"):?> bgcolor="#FFF766"<?php endif; ?>>新<br>品</td>
 		<td<?php if($_POST['condition'] == "NS"):?> bgcolor="#FFF766"<?php endif; ?>>未<br>使<br>用<br>品</td>
-		<td<?php if($_POST['condition'] == "S"):?> bgcolor="#FFF766"<?php endif; ?>>新<br>品<br>同<br>様<br>品</td>
+		<td<?php if($_POST['condition'] == "S"):?> bgcolor="#FFF766"<?php endif; ?>>展<br>示<br>品<br>ま<br>た<br>は<br>ア<br>ウ<br>ト<br>レ<br>ッ<br>ト<br>商<br>品</td>
+		<td<?php if($_POST['condition'] == "SA"):?> bgcolor="#FFF766"<?php endif; ?>>新<br>品<br>同<br>様<br>品</td>
 		<td<?php if($_POST['condition'] == "A"):?> bgcolor="#FFF766"<?php endif; ?>>中<br>古<br>超<br>美<br>品</td>
 		<td<?php if($_POST['condition'] == "AB"):?> bgcolor="#FFF766"<?php endif; ?>>中<br>古<br>美<br>品</td>
 		<td<?php if($_POST['condition'] == "B"):?> bgcolor="#FFF766"<?php endif; ?>>中<br>古<br>良<br>品</td>

bca633e7ba9d70abbfa2cd2cab6b6e3976f5603e
DR Subordinate <dr_subordinate@example.com>
2024-11-14 21:06:55 +0900

    Yahoo!ショッピング スマホ用説明文の修正
    NS(未使用展示品)のランク削除に伴い
    Yahoo!ショッピング スマホ用説明文のタグからもNS(未使用展示品)を削除

--- tags/yahoo-shopping-sp-description.php
+++ tags/yahoo-shopping-sp-description.php
@@ -177,7 +177,7 @@ switch ($_POST['country-of-manufacture']) {
 <img src="https://shopping.c.yimg.jp/lib/brand-across/title_item_2.jpg" style="vertical-align: bottom" alt="商品ランク" width="100%">
 <table class="tblitemrank" width="100%" style="border-collapse:collapse;">
 <tr>
-<th width="20%" bgcolor="#EBE8D7" style="padding:8px;border:#dddddd solid 1px;font-size:24px;font-weight:bold;color:#bf0000;"><?php echo ($_POST['condition'] === 'NS-display-item') ? 'NS' : $_POST['condition']; ?></th>
+<th width="20%" bgcolor="#EBE8D7" style="padding:8px;border:#dddddd solid 1px;font-size:24px;font-weight:bold;color:#bf0000;"><?php echo $_POST['condition']; ?></th>
 <td width="80%" bgcolor="#ffffff" style="padding:8px;border:#dddddd solid 1px;">
 <?php 
 switch ($_POST['condition']) {
@@ -186,9 +186,6 @@ switch ($_POST['condition']) {
         break;
     case "NS":
         echo "新品、未使用品 製造から2年以上経過、又は製造年が不明な商品";
-        break;
-    case "NS-display-item":
-        echo "未使用展示品";
         break;
     case "S":
         echo "新品同様品";
@@ -253,7 +250,7 @@ switch ($_POST['condition']) {
 </tr>
 <tr>
 <td<?php if($_POST['condition'] == "N"):?> bgcolor="#FFF766"<?php endif; ?> style="text-align:center;border:#dddddd solid 1px;">新<br>品</td>
-<td<?php if($_POST['condition'] == "NS" || $_POST['condition'] == "NS-display-item"):?> bgcolor="#FFF766"<?php endif; ?> style="text-align:center;border:#dddddd solid 1px;"><?php echo ($_POST['condition'] == "NS") ? "未<br>使<br>用<br>品" : (($_POST['condition'] == "NS-display-item") ? "未<br>使<br>用<br>展<br>示<br>品" : "未<br>使<br>用<br>品"); ?></td>
+<td<?php if($_POST['condition'] == "NS"):?> bgcolor="#FFF766"<?php endif; ?> style="text-align:center;border:#dddddd solid 1px;">未<br>使<br>用<br>品</td>
 <td<?php if($_POST['condition'] == "S"):?> bgcolor="#FFF766"<?php endif; ?> style="text-align:center;border:#dddddd solid 1px;">新<br>品<br>同<br>様<br>品</td>
 <td<?php if($_POST['condition'] == "A"):?> bgcolor="#FFF766"<?php endif; ?> style="text-align:center;border:#dddddd solid 1px;">中<br>古<br>超<br>美<br>品</td>
 <td<?php if($_POST['condition'] == "AB"):?> bgcolor="#FFF766"<?php endif; ?> style="text-align:center;border:#dddddd solid 1px;">中<br>古<br>美<br>品</td>
@@ -267,9 +264,6 @@ switch ($_POST['condition']) {
 <br><br><img src="https://shopping.c.yimg.jp/lib/brand-across/newitem_notes.jpg" width="100%">
 <?php endif; ?>
 <?php if($_POST['condition'] == "NS"): ?>
-<br><br><img src="https://shopping.c.yimg.jp/lib/brand-across/newitem_notes.jpg" width="100%">
-<?php endif; ?>
-<?php if($_POST['condition'] == "NS-display-item"): ?>
 <br><br><img src="https://shopping.c.yimg.jp/lib/brand-across/newitem_notes.jpg" width="100%">
 <?php endif; ?>
 <br><br>

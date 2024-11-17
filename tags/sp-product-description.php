8fcb2e844a3d1673c456a3eea960f64a1af68e16
DR Subordinate <dr_subordinate@example.com>
2024-11-17 17:31:31 +0900

    スマホ用商品説明文の修正
    商品ランクの変更と商品ランクの説明に<b>タグと<br>タグを追加

--- tags/sp-product-description.php
+++ tags/sp-product-description.php
@@ -210,7 +210,6 @@ switch ($_POST['country-of-manufacture']) {
 <td colspan="2" bgcolor="#fff">
 <table border="1" cellspacing="0" cellpadding="7" width="100%" bordercolor="#999">
 	<td align="center" width="11%"<?php if($_POST['condition'] == "N"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>N</td>
-	<td align="center" width="11%"<?php if($_POST['condition'] == "NS"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>NS</td>
 	<td align="center" width="11%"<?php if($_POST['condition'] == "S"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>S</td>
 	<td align="center" width="11%"<?php if($_POST['condition'] == "SA"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>SA</td>
 	<td align="center" width="11%"<?php if($_POST['condition'] == "A"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>A</td>
@@ -218,7 +217,6 @@ switch ($_POST['country-of-manufacture']) {
 	<td align="center" width="11%"<?php if($_POST['condition'] == "B"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>B</td>
 	<td align="center" width="11%"<?php if($_POST['condition'] == "BC"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>BC</td>
 	<td align="center" width="11%"<?php if($_POST['condition'] == "C"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>C</td>
-	<td align="center" width="11%"<?php if($_POST['condition'] == "J"):?> bgcolor="#FFF766"<?php else:?> bgcolor="#fff"<?php endif; ?>>D</td>
 </table>
 </td>
 </tr>
@@ -230,34 +228,28 @@ switch ($_POST['country-of-manufacture']) {
 
 switch ($_POST['condition']) {
     case "N":
-        echo "新品※金具部分など輸送・検品の際にどうしてもついてしまう僅かなスレ傷がある場合がございます。";
-        break;
-    case "NS":
-        echo "未使用品 付属品に欠品あり又は製造年が不明な商品※保管による僅かなダメージがある場合がございます。";
+        echo "<b>新品</b><br>※金具部分など輸送・検品の際にどうしてもついてしまう僅かなスレ傷がある場合がございます。";
         break;
     case "S":
-        echo "展示品またはアウトレット商品 未使用品だが長期保管や展示により明確なダメージ等がある新古商品";
+        echo "<b>未使用品（展示品またはアウトレット商品含む）</b><br>未使用品だが長期保管や展示により明確なダメージ等がある商品。アウトレット商品も含まれます。※詳細は下記説明をご覧ください";
         break;
     case "SA":
-        echo "新品同様品 細かなあまり目立たないダメージがある程度の新品同様に奇麗なUSED商品";
+        echo "<b>新品同様品</b><br>細かなあまり目立たないダメージがある程度の新品同様に奇麗なUSED商品";
         break;
     case "A":
-        echo "数回使用程度の美品";
+        echo "<b>数回使用程度の美品</b>";
         break;
     case "AB":
-        echo "若干の小傷又はシミがあるが全体的には状態が良い";
+        echo "<b>若干の小傷又はシミがあるが全体的には状態が良い</b>";
         break;
     case "B":
-        echo "一般的な中古品で全体的に使用感がある";
+        echo "<b>一般的な中古品で全体的に使用感がある</b>";
         break;
     case "BC":
-        echo "全体的に強い使用感がある";
+        echo "<b>全体的に強い使用感がある</b>";
         break;
     case "C":
-        echo "強い使用感あり　劣化や大きい汚れなどがある";
-        break;
-    case "D":
-        echo "欠品、故障有り";
+        echo "<b>強い使用感あり　劣化や大きい汚れなどがある</b>";
         break;
 }
  ?>

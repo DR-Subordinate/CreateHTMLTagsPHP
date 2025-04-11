0b4d9dfc4dd0802d58878353b388925ccb24530c
DR Subordinate <karin.pspink@gmail.com>
2025-04-11 12:21:21 +0900

    スマホ用商品説明文の修正
    ランクのテーブルの文言を削除

--- tags/sp-product-description.php
+++ tags/sp-product-description.php
@@ -281,10 +281,6 @@ switch ($_POST['condition']) {
 <td bgcolor="#FFF"><?php echo nl2br($_POST['syousai']); ?></td>
 </tr>
 <?php endif; ?>
-<tr>
-<td bgcolor="#EEE"></td>
-<td bgcolor="#FFF">※ランクは目安となりますので、上部詳細説明及び青いバナー内の詳細画像をご確認下さいませ。</td>
-</tr>
 </table>
 <br>
 
